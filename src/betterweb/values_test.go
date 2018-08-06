b524b3123397ca6d2b6dad2fe85c4e093ed803ad
Tal Riginiano <tal@talreg.com>
2018-08-06 01:37:08 -0400

    fixed test

--- src/betterweb/values_test.go
+++ src/betterweb/values_test.go
@@ -7,9 +7,8 @@ import (
 
 func TestTimeValues(t *testing.T) {
 	t1 := time.Now()
-	t2 := time.Now()
-	t1.Add(time.Minute * 1)
-	t3 := t1.Sub(t2)
+	t2 := t1.Add(time.Minute * 1)
+	t3 := t2.Sub(t1)
 	if t3.Minutes() != 1 {
 		t.Errorf("time difference failure: should be 1, but it is %f", t3.Minutes())
 	}
