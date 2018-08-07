dbf63d0d86859244ef9078be62b94a8bc7a63760
Tal Riginiano <tal@talreg.com>
2018-08-06 20:26:15 -0400

    update

--- src/aws-utils/main.go
+++ src/aws-utils/main.go
@@ -16,7 +16,7 @@ func main() {
 		logging.RecordLogLine(fmt.Sprintf("%v while creating a session", err))
 		os.Exit(1)
 	}
-	logging.RecordLogLine("monitor starting")
+	logging.RecordLogLine("monitor starting - version 1.0.0.1")
 	server := betterweb.CreateHealthCheckServer()
 	server.SetSession(sess)
 	logging.RecordLogLine("monitor started.\r\n")
